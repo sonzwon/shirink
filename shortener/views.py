e23db8800b213b063737b13c60a508c0f12e28ed
sonzwon <sby04056@gmail.com>
2023-03-12 23:00:43 +0900

    get,post

--- shortener/views.py
+++ shortener/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from shortener.models import Users
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def index(request):
@@ -13,8 +15,22 @@ def index(request):
     # print(email)
     return render(request, "base.html", {"welcome_msg" : f"Hello {email}"})
 
-def redirect_test(requset):
-    print("Go Redirect")
-    return redirect("index")
+# def redirect_test(requset):
+#     print("Go Redirect")
+#     return redirect("index")
 
+@csrf_exempt
+def get_user(request, user_id):
+    print(user_id)
+    if request.method == "GET":
+        abc = request.GET.get("abc")
+        xyz = request.GET.get("xyz")
+        user = Users.objects.filter(pk=user_id).first()
+        return render(request, "base.html", {"user":user, "params":[abc, xyz]})
+    elif request.method == "POST":
+        username = request.GET.get("username")
+        if username:
+            user = Users.objects.filter(pk=user_id).update(username=username)
+        
+        return JsonResponse(dict(msg="You just reached with Post Method!"))
 
