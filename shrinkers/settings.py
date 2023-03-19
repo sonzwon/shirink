687d71be34de2e3e1eaf269686e48019aed05bf7
sonzwon <sby04056@gmail.com>
2023-03-19 19:41:29 +0900

    custom_tags

--- shrinkers/settings.py
+++ shrinkers/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'shrinkers.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templetes")],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
