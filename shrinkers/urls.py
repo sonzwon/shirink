53d799dfdb364c8da3cc51e0dab849ff895820af
sonzwon <sby04056@gmail.com>
2023-03-27 14:12:44 +0900

    CRUD

--- shrinkers/urls.py
+++ shrinkers/urls.py
@@ -17,7 +17,17 @@ import debug_toolbar
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
-from shortener.views import index, get_user, register, login_view, logout_view, list_view, url_list, url_create
+from shortener.views import (
+    index,
+    get_user,
+    register,
+    login_view,
+    logout_view,
+    list_view,
+    url_list,
+    url_create,
+    url_change,
+)
 
 
 urlpatterns = [
@@ -29,6 +39,7 @@ urlpatterns = [
     path("login/", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
     path("list/", list_view, name="list_view"),
-    path("url_list", url_list, name="url_list"),
-    path("url_create", url_create, name="url_create"),
+    path("urls", url_list, name="url_list"),
+    path("urls/create", url_create, name="url_create"),
+    path("urls/<str:action>/<int:url_id>", url_change, name="url_change"),
 ]
